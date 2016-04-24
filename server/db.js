5434400453f29bc3b8cbb136002c16f8a8a1b29d
Jane Fong <janeefongg@gmail.com>
2016-04-24 15:34:06 -0700

    [setup] database connection and schema

---
+++ server/db.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const dbUrl = 'mongodb:localhost/test';
+const db = mongoose.connection;
+
+const Schema = mongoose.Schema;
+
+var ClothingSchema = new Schema({
+  name: String,
+  price: String,
+  description: String,
+  condition: String,
+  image: String
+});
+
+var Clothing = mongoose.model('Clothing', ClothingSchema);
+
+mongoose.connect(dbUrl);
+
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', function () {
+  console.log('Connected to database');
+});
+
+module.exports = Clothing;
